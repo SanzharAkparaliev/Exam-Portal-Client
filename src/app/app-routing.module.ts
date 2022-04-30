437a770e3472e4ffec1afbeb782feaffa14b77dd
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-30 11:22:26 +0600

    creating registration form

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+
+const routes:Routes = [
+{
+  path:'signup',
+  component:SignupComponent,
+  pathMatch:'full',
+},
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import { SignupComponent } from './pages/signup/signup.component';
+
+
+@NgModule({
+  imports:[
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  declarations:[SignupComponent],
+})
+export class MaterialModule {};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
