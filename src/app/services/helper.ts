d3b03fc7736485325eadb0e009b15e4d41eb0310
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-30 12:30:46 +0600

    send registration user data to server

---
+++ src/app/services/helper.ts
@@ -0,0 +1,2 @@
+let baseUrl = 'http://localhost:8000';
+export default baseUrl;
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) { }
+
+  public addUser({ user }: { user: any; }){
+    return this.http.post(`${baseUrl}/user`,user)
+  }
+}
