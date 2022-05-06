310e64cfb94df64cf55d6796cf28eb3ba0ecf336
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-05-06 19:28:22 +0600

    displaying only active quizzes

--- src/app/pages/user/load-quiz/load-quiz.component.ts
+++ src/app/pages/user/load-quiz/load-quiz.component.ts
@@ -33,7 +33,7 @@ export class LoadQuizComponent implements OnInit {
 
 
     if(this.catId == 0){
-      this._quiz.quizzes().subscribe(
+      this._quiz.getActiveQuizzes().subscribe(
         (data:any) =>{
           this.quizzes = data;
           console.log(this.quizzes)
@@ -42,7 +42,7 @@ export class LoadQuizComponent implements OnInit {
         }
       )
     }else{
-      this._quiz.getQuizOfCategory(this.catId).subscribe(
+      this._quiz.getActiveQuizzesOfCategory(this.catId).subscribe(
         (data:any) => {
           this.quizzes = data
         },(error) => {
